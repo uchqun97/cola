8aa8cda460165ca5240a118a77372b8bc0b6e9b7
uchqun97 <uchqun1997shoh@gmail.com>
2024-11-04 05:22:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// let cola =50;
+    
+// let qabulQilinganTangalar = [5, 10, 25];
+
+// for (let colaSora; cola > 0;) {
+//     colaSora = +(prompt('Cola olish uchun faqat 5, 10, 25 centlik tangalar kirgiz'));
+    
+//     if (qabulQilinganTangalar.includes(colaSora)) {
+//         cola -= colaSora;
+
+//         if (cola < 0) {
+//             alert('Bu pul ortiqcha!');
+//             cola += colaSora; 
+//         } else {
+//             alert(`Qolgan summa: ${cola}`);
+//         }
+//     } else {
+//         alert('Notogri qiymat');
+//     }
+// }
+
+// alert('Pul tugadi, rahmat!');
+
+
+
+
+// let sonlar = [-20, 5.24, 152, -30, 45.5];
+// let s = 0;
+
+// for (let i = 0; i < sonlar.length; i++) {
+//     s += sonlar[i];
+// }
+
+// console.log(`Masivdagi sonlar yig'indisi: ${s}`);
+
+
+
+
+// const numbers = [1,25, -19,20,0,2.4];
+// numbers.reverse();
+
+// console.log(numbers); 
+
+
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function(a, b) { return a - b });
+// console.log(points);
+
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function(a, b) { return b-a });
+// console.log(points);
